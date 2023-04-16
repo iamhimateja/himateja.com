e78c15b6b4fd07d37eebce7dc769558270fbadb0
Himateja Merlapaka <iamhimateja@gmail.com>
2023-04-16 20:06:38 +0530

    Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/colors.css'
 import '@/styles/variables.css'
 
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 import { Footer, Navigation } from '@/components'
 import { useArrowKeyNavigation, useThemeSettings } from '@/hooks'
@@ -13,6 +14,20 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-GT78JPR2RG" />
+
+      <Script strategy="lazyOnload" id="googleAnalyticsScript">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-GT78JPR2RG', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Navigation />
       <Component {...pageProps} />
       <Footer />
