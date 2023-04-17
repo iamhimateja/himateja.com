b588cb885fd9dd7245804f804ecbcb712053f6ec
Himateja Merlapaka <iamhimateja@gmail.com>
2023-04-17 20:31:00 +0530

    Updated about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -16,34 +16,32 @@ export default function About() {
             <span>A bit about myself</span>
           </div>
           <p className={styles.paragraph}>
-            Himateja Merlapaka is a full-stack engineer who loves playing with frontend technologies.
+            I am a full-stack engineer who is passionate about exploring and experimenting with frontend technologies
           </p>
           <p className={styles.paragraph}>
-            Throughout his journey from personal projects to collaborating with established companies, Hima has
-            sharpened his skills in web development across various applications, like UI/UX design, frontend stuff, and
-            backend development using Node.js, React and Ruby on Rails.
+            Through my journey from personal projects to working with established companies, I have honed my skills in
+            web development across various applications such as UI/UX design, frontend development using Node.js, React
+            and Ruby on Rails, and backend development.
           </p>
           <p className={styles.paragraph}>
-            One of Hima&#39;s strengths lies is his knack for understanding project requirements and transforming them
-            into smooth, efficient solutions that accommodate both users and businesses alike.
+            One of my strengths lies is his knack for understanding project requirements and transforming them into
+            smooth, efficient solutions that accommodate both users and businesses alike.
           </p>
           <p className={styles.paragraph}>
-            Besides being a coding nerd, Hima is a strong communicator and team player. He loves engaging with
-            cross-functional teams, including designers, product managers, and stakeholders, ensuring that the final
-            product vibes with the goals and vision of everyone involved. Always on the lookout for new opportunities,
-            Hima is keen to connect and work with fellow creative minds to craft innovative and user-centric digital
-            experiences.
+            Besides being a coding nerd, I am an excellent communicator and team player. I enjoy collaborating with
+            cross-functional teams, including designers, product managers, and stakeholders, to ensure that the final
+            product aligns with everyone&#39;s goals and vision. I am always on the lookout for new opportunities to
+            connect and work with creative minds to develop innovative and user-centric digital experiences.
           </p>
           <p className={styles.paragraph}>
-            When he&#39;s not coding, Hima enjoys watching movies, playing video games, and spending time with his
-            family.
+            When I am not coding, I enjoy watching movies, playing video games, and spending time with my family.
           </p>
           <p className={styles.paragraph}>
             Here is a full{' '}
             <Link href="/files/Himateja - Resume.pdf" target="_blank" aria-label="Download hima's résumé" tabIndex={0}>
               résumé
             </Link>{' '}
-            of Hima&#39;s work experience and education.
+            of my work experience and education.
           </p>
         </div>
         <div className={styles.content}>
